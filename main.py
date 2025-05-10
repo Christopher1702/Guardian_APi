680627039676a4401b0da2a2504c86adb41161ac
Christopher1702 <christopher.dherman2317@gmail.com>
2025-05-10 13:36:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import io
 
 #----------------------------------------------------------------------------------------
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
-model = genai.GenerativeModel('gemini-2.0-flash')
+model = genai.GenerativeModel('gemini-2.0-flash-lite')
 load_dotenv()
 cred_path = os.getenv("FIREBASE_CREDENTIALS_PATH")
 cred = credentials.Certificate(cred_path)
